27168fce61e9ef987a50db2905601ef5cc5f77e6
kennychiangcom <kenny@chiang.com>
2022-09-12 04:59:37 +0100

    Done converting valid roman numerals with tests, invalid roman numerals fail tests.

--- RomanNumeralsApp/RomanNumeralsConverter.cs
+++ RomanNumeralsApp/RomanNumeralsConverter.cs
@@ -11,10 +11,45 @@ namespace RomanNumeralsApp
         public static void Main()
         {
         }
+
         public int RConvert(string num)
         {
-            num = num;
-            return -1;
+            num = num.ToUpper().Trim();
+            int[] numValue = new int[num.Length];
+            for (int i = 0; i < num.Length; i++)
+            {
+                switch (num.Substring(i, 1))
+                {
+                    case "M":
+                        numValue[i] = 1000;
+                        break;
+                    case "D":
+                        numValue[i] = 500;
+                        break;
+                    case "C":
+                        numValue[i] = 100;
+                        break;
+                    case "L":
+                        numValue[i] = 50;
+                        break;
+                    case "X":
+                        numValue[i] = 10;
+                        break;
+                    case "V":
+                        numValue[i] = 5;
+                        break;
+                    case "I":
+                        numValue[i] = 1;
+                        break;
+                    default:
+                        return -1;
+                }
+            }
+            for (int i = 0; i < numValue.Length - 1; i++)
+            {
+                if (numValue[i] < numValue[i + 1]) numValue[i] = -numValue[i];
+            }
+            return numValue.Sum();
         }
     }
 }
