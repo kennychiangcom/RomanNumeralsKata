6b2db94c396f06bf55251df2a686675c3a381a24
kennychiangcom <kenny@chiang.com>
2022-09-12 04:53:45 +0100

    Initial test passes on checking invalid string entered and fails on valid strings.

---
+++ RomanNumeralsApp.Tests/RomanNumeralsConverterTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsApp.Tests
+{
+    public class RomanNumeralsConverterTest
+    {
+        private RomanNumeralsConverter _romanNumeralsConverter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _romanNumeralsConverter = new RomanNumeralsConverter();
+        }
+
+        [Test]
+        public void TestConvertingValidNumerals()
+        {
+            _romanNumeralsConverter.RConvert("i").Should().Be(1);
+            _romanNumeralsConverter.RConvert("III").Should().Be(3);
+            _romanNumeralsConverter.RConvert("iv").Should().Be(4);
+            _romanNumeralsConverter.RConvert("vi").Should().Be(6);
+            _romanNumeralsConverter.RConvert("viii").Should().Be(8);
+            _romanNumeralsConverter.RConvert("MCMLXXXix").Should().Be(1989);
+            _romanNumeralsConverter.RConvert("MMMDCCCLXXXVIII").Should().Be(3888);
+        }
+        [Test]
+        public void TestConvertingInvalidNumerals()
+        {
+            _romanNumeralsConverter.RConvert("iiii").Should().Be(-1);
+            _romanNumeralsConverter.RConvert("vv").Should().Be(-1);
+            _romanNumeralsConverter.RConvert("xXxX").Should().Be(-1);
+            _romanNumeralsConverter.RConvert("LL").Should().Be(-1);
+            _romanNumeralsConverter.RConvert("CCCC").Should().Be(-1);
+            _romanNumeralsConverter.RConvert("DD").Should().Be(-1);
+            _romanNumeralsConverter.RConvert("MMMM").Should().Be(-1);
+        }
+    }
+}
---
+++ RomanNumeralsApp/RomanNumeralsConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsApp
+{
+    public class RomanNumeralsConverter
+    {
+        public static void Main()
+        {
+        }
+        public int RConvert(string num)
+        {
+            num = num;
+            return -1;
+        }
+    }
+}
